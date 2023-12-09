e75903516e6dcbe0592baec7fc0341f3bd5328b8
PaperTrain05 <michelemameli5@gmail.com>
2023-12-09 20:42:56 +0100

    Initial commit

---
+++ src/main/java/it/mandress101/sac/SAC_AD.java
@@ -0,0 +1,59 @@
+package it.mandress101.sac;
+
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import it.mandress101.sac.listener.TabListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public final class SAC_AD extends JavaPlugin {
+    private static SAC_AD pl;
+    public static ProtocolManager manager;
+    public static File txt;
+    public boolean isLogs;
+
+    @Override
+    public void onEnable() {
+        pl = this;
+        if(Bukkit.getPluginManager().getPlugin("ProtocolLib") == null) {
+            getLogger().severe("PROTOCOL LIB IS NULL DISABLING PLUGIN");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        if(Bukkit.getPluginManager().getPlugin("SAC") == null) {
+            getLogger().severe("SAC IS NULL DISABLING PLUGIN");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+        manager = ProtocolLibrary.getProtocolManager();
+        File configFile = new File(getDataFolder(), "config.yml");
+        if(!configFile.exists()) {
+            saveDefaultConfig();
+        }
+        getConfig().options().copyDefaults(true);
+        txt = new File(getDataFolder(), "logs.txt");
+        isLogs = getConfig().getBoolean("features.anti-tab-complete.logs.enabled");
+        if(isLogs) {
+            if(!txt.exists()) {
+                try {
+                    txt.createNewFile();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        new TabListener(this, isLogs);
+    }
+
+    public static SAC_AD getPlugin() {
+        return pl;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/it/mandress101/sac/listener/TabListener.java
@@ -0,0 +1,70 @@
+package it.mandress101.sac.listener;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import it.mandress101.sac.SAC_AD;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TabListener {
+    private final Map<String, Boolean> allowed;
+    private final boolean isLogs2;
+
+    public TabListener(Plugin plugin, boolean isLogs) {
+        this.isLogs2 = isLogs;
+        this.allowed = new HashMap<>();
+        List<String> commands = SAC_AD.getPlugin().getConfig().getStringList("features.anti-tab-complete.list");
+        for(String command : commands) {
+            allowed.put(command, true);
+        }
+
+        SAC_AD.manager.addPacketListener(new PacketAdapter(plugin, ListenerPriority.MONITOR, PacketType.Play.Client.TAB_COMPLETE) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+
+                String inculati = event.getPacket().getStrings().read(0).toLowerCase();
+
+                if(inculati.startsWith("/")) {
+                    String commandDIO = inculati.split(" ")[0].substring(1);
+
+                    if(!allowed.containsKey(commandDIO)) {
+                        event.setCancelled(true);
+                    }
+                    if(isLogs2) {
+                        String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
+                        String playerName = event.getPlayer().getName();
+                        String format = playerName + " has flagged TabListener in date " + time + " command: " + commandDIO;
+                        saveLog(format);
+                    }
+                }
+            }
+        });
+    }
+
+    public void saveLog(String format) {
+        try {
+            File dataFolder = SAC_AD.getPlugin().getDataFolder();
+            if(!dataFolder.exists()) {
+                dataFolder.mkdirs();
+            }
+            FileWriter fw = new FileWriter(SAC_AD.txt, true);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println(format);
+            pw.flush();
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
